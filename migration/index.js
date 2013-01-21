c88c4fdd0a21e195eb3954400e3025ac3621b82a
mrzmyr <m0ritz.m3yer@gmail.com>
2013-01-21 22:03:09 +0100

    improved migration script

--- migration/index.js
+++ migration/index.js
@@ -17,13 +17,13 @@ fs.exists(config.uploadDir, function (exists) {
     if(!exists) fs.mkdir(config.uploadDir);
 });
 
-var seq = new fut();
+var assetsSeq = new fut();
 
 fs.readdir(assetDir, function (err, files) {
 
     files.forEach(function (file) {
 
-        seq.then(function (next) {
+        assetsSeq.then(function (next) {
 
             var req  = request.post(config.url + '/meals/');
             var form = req.form()
@@ -41,22 +41,47 @@ fs.readdir(assetDir, function (err, files) {
     });
 });
 
-var mealtimes = [
+var mealtimesMigration = [
     { id: 'mealtime_1', title: 'A - 12:30'},
     { id: 'mealtime_2', title: 'B - 13:15'},
     { id: 'mealtime_3', title: 'C - 14:00'},
 ];
 
-mealtimes.forEach(function (item) {
-    request({
-        uri     :  config.url + '/mealtimes/',
-        method  : 'POST',
-        body    : {
-            id   : item.id,
-            title: item.title
-        },
-        json: true
-    }, function () {
-        console.log('Added mealtime: ' + item.id + ' ' + item.title);
-    });
-});
+var mealtimeSeq = new fut();
+
+mealtimeSeq
+
+    .then(function (next) {
+
+        request({
+            uri     :  config.url + '/mealtimes/',
+            method  : 'GET',
+            json: true
+        }, function (error, response, mealtimes) {
+            if(mealtimes.length < mealtimesMigration.length) {
+                next();
+            } else {
+                console.log('mealtimes already exists.');
+            }
+        });
+
+    })
+
+    .then(function () {
+
+        mealtimesMigration.forEach(function (item) {
+            request({
+                uri     :  config.url + '/mealtimes/',
+                method  : 'POST',
+                body    : {
+                    id   : item.id,
+                    title: item.title
+                },
+                json: true
+            }, function () {
+                console.log('Added mealtime: ' + item.id + ' ' + item.title);
+            });
+        });
+    })
+
+
