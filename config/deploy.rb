680a474a3051463f149879abc13d8286f7fca598
Axel Kelting <axel.kelting@icans-gmbh.com>
2013-09-09 11:16:59 +0200

    - capify for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, 'Trending Food'
+set :repo_url, 'https://github.com/ICANS/trending-food.git'
+set :branch, 'master'
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :scm, :git
+set :deploy_via, :remote_cache
+
+# set :format, :pretty
+set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+
+after 'deploy:updated', "npm:install"
