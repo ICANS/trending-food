18171634134c3e89d31e6bc76cb59a175af44172
Axel Kelting <axel.kelting@icans-gmbh.com>
2013-09-09 11:23:55 +0200

    - add cap stages

---
+++ config/deploy/staging.rb
@@ -0,0 +1,43 @@
+set :stage, :staging
+set :deploy_to, '/var/www/trending-food-stage'
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+role :app, %w{deploy@tf.ham.idn5.org}
+role :web, %w{deploy@tf.ham.idn5.org}
+# role :db,  %w{deploy@example.com}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a has can be used to set
+# extended properties on the server.
+server 'tf.ham.idn5.org', user: 'deploy', roles: %w{web, app}, keys: '~/.ssh/id_rsa'
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
+
+# fetch(:default_env).merge!(:rails_env, :staging)
